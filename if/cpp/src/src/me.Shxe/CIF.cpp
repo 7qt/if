b7a145b09027ec917e6e2c250e263543ecc1cf9d
Shae <69154762+Sh4e@users.noreply.github.com>
2021-02-04 15:06:22 +0000

    04/02/2021
    added first file update I will update when I learn to code lmao

---
+++ if/cpp/src/src/me.Shxe/CIF.cpp
@@ -0,0 +1,36 @@
+
+/*
+* Auther : Shxe
+* Created : 04/02/2021
+* Updated : N/a
+* Desc : Made for fun and why not make it... also I don't know how to code in cpp or c so this is just gonna be copy paste lmao 
+*        if anyone can help me with this to make it actually a thing dm me on discord @ lns4nlty#9786
+*/
+
+if (a = b) {
+    if (c = d) {
+        if (e = f) {
+            if (g = h) {
+                if (i = j) {
+                    if (k = l) {
+                        if (m = n) {
+                            if (o = p) {
+                                if (q = r) {
+                                    if (s = t) {
+                                        if (u = v) {
+                                            if (w = x) {
+                                                if (y = z) {
+                                                    std::cout << "I have issues and I know it :) c++ strong coder man" << std::endl;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
---
+++ if/java/src/src/Me/Shxe/JIF.java
@@ -0,0 +1,70 @@
+package Me.Shxe;
+
+/*
+* Auther : Shxe
+* Created : 04/02/2021
+* Updated : N/a
+* Desc : Made this for fun since my friend made it in c++ and also I wanted to get my public repo stuff dont lol
+*        don't repaste this code since it is completely useless :') also gonna do this in c++ , c , kotlin and
+*        many others I guess until I get bored.
+*/
+
+public class JIF {
+
+    public void Abc () {
+        if(a = b){
+            if(c = d){
+                if(e = f){
+                    if(g = h){
+                        if(i = j){
+                            if(k = l){
+                                if(m = n){
+                                    if(o = p){
+                                        if(q = r){
+                                            if(s = t){
+                                                if(u = v){
+                                                    if (w = x) {
+                                                        if(y = z){
+                                                            // this is strong code trust me. - Shxe 04/02/2021
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean a;
+    public static boolean b;
+    public static boolean c;
+    public static boolean d;
+    public static boolean e;
+    public static boolean f;
+    public static boolean g;
+    public static boolean h;
+    public static boolean i;
+    public static boolean j;
+    public static boolean k;
+    public static boolean l;
+    public static boolean m;
+    public static boolean n;
+    public static boolean o;
+    public static boolean p;
+    public static boolean q;
+    public static boolean r;
+    public static boolean s;
+    public static boolean t;
+    public static boolean u;
+    public static boolean v;
+    public static boolean w;
+    public static boolean x;
+    public static boolean y;
+    public static boolean z;
+}
