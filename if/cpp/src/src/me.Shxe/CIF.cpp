9d4402e9e53f02df85193825f5aec5be8ba96269
7qt <45207596+7qt@users.noreply.github.com>
2021-03-02 17:13:07 +0000

    Cpp file fix
    Added variables required and library needed for code to run, still wont work as I cba to add a value for each variable but is more realistic now :/

--- if/cpp/src/src/me.Shxe/CIF.cpp
+++ if/cpp/src/src/me.Shxe/CIF.cpp
@@ -5,22 +5,30 @@
 * Updated : N/a
 * Desc : Made for fun and why not make it... also I don't know how to code in cpp or c so this is just gonna be copy paste lmao 
 *        if anyone can help me with this to make it actually a thing dm me on discord @ lns4nlty#9786
+*
+* Edited by 7qt (Silverkill) on 02/03/2021
 */
 
-if (a = b) {
-    if (c = d) {
-        if (e = f) {
-            if (g = h) {
-                if (i = j) {
-                    if (k = l) {
-                        if (m = n) {
-                            if (o = p) {
-                                if (q = r) {
-                                    if (s = t) {
-                                        if (u = v) {
-                                            if (w = x) {
-                                                if (y = z) {
-                                                    std::cout << "I have issues and I know it :) c++ strong coder man" << std::endl;
+#include <iostream>
+
+int main(){
+    int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+    
+    if (a = b) {
+        if (c = d) {
+            if (e = f) {
+                if (g = h) {
+                    if (i = j) {
+                        if (k = l) {
+                            if (m = n) {
+                                if (o = p) {
+                                    if (q = r) {
+                                        if (s = t) {
+                                            if (u = v) {
+                                                if (w = x) {
+                                                    if (y = z) {
+                                                        std::cout << "I have issues and I know it :) c++ strong coder man" << std::endl;
+                                                    }
                                                 }
                                             }
                                         }
@@ -33,4 +41,5 @@ if (a = b) {
             }
         }
     }
-};
+    return 0;
+}
