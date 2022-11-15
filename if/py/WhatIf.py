f493afc2515607e4791279a7fd8958ba79b0a845
June <89613516+qe7@users.noreply.github.com>
2022-11-15 23:43:29 +0000

    Create WhatIf.py
    Python > all

---
+++ if/py/WhatIf.py
@@ -0,0 +1,54 @@
+a = True
+b = True
+c = True
+d = True
+e = True
+f = True
+g = True
+h = True
+i = True
+j = True
+k = True
+l = True
+m = True
+n = True
+o = True
+p = True
+q = True
+r = True
+s = True
+t = True
+u = True
+v = True
+w = True
+x = True
+y = True
+z = True
+
+if a == b:
+    if b == c:
+        if c == d:
+            if d == e:
+                if e == f:
+                    if f == g:
+                        if g == h:
+                            if h == i:
+                                if i == j:
+                                    if j == k:
+                                        if k == l:
+                                            if l == m:
+                                                if m == n:
+                                                    if n == o:
+                                                        if o == p:
+                                                            if p == q:
+                                                                if q == r:
+                                                                    if r == s:
+                                                                        if s == t:
+                                                                            if t == u:
+                                                                                if u == v:
+                                                                                    if v == w:
+                                                                                        if w == x:
+                                                                                            if x == y:
+                                                                                                if y == z:
+                                                                                                    print("I have issues and I know it :) python strong coder man")
+
